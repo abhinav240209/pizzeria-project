c5fc2d8425241e6800b4fb6b2bbc20a01a14e0dd
abhinav240209 <83449563+abhinav240209@users.noreply.github.com>
2021-07-25 09:47:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var menu_list_array = ["Veg Margherita Pizza","Pepperoni Pizza","Meat Pizza","Margherita Pizza","BBQ Chicken Pizza","Hawaiian Pizza","Buffalo Pizza"];
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>";
+menu_list_array.sort();
+console.log(menu_list_array)
+for(var i=0;i<menu_list_array.length;i++) {
+    htmldata=htmldata +"<li>"+ menu_list_array[i] +"</li>";
+   
+}
+
+htmldata=htmldata+"</ol>";
+document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<section class='cards'>";
+for(var i=0;i<menu_list_array.length;i++) {
+    htmldata=htmldata +'<div class="card">'+ '<img src="images/pizzaIMG.png"/>' + menu_list_array[i]+ '</div>';
+}
+htmldata=htmldata+"</section>";
+document.getElementById("display_addedmenu").innerHTML=htmldata;
+}
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    add_item();
+}
